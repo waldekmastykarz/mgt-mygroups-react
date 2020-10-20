b3e7bfa39f3b47c808b8768c3171154ba736b316
waldekmastykarz <waldek@mastykarz.nl>
2020-10-20 13:06:44 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Persona, PersonaSize, Spinner } from '@fluentui/react';
+import { MsalProvider, Providers, ProviderState, TemplateHelper } from '@microsoft/mgt';
+import { Get, Login, MgtTemplateProps } from '@microsoft/mgt-react';
+import { ResponseType } from '@microsoft/microsoft-graph-client';
+import { Group } from '@microsoft/microsoft-graph-types';
+import React, { useState } from 'react';
 import './App.css';
 
+const GroupInfo = (props: MgtTemplateProps) => {
+  const [imageUrl, setGroupImage] = useState('');
+  const url = window.URL || window.webkitURL;
+
+  const group: Group = props.dataContext;
+  const openGroup = (event: any) => window.open(`https://outlook.office365.com/mail/group/${group.mail?.substr(group.mail.indexOf('@') + 1)}/${group.mailNickname}/email`, '_blank');
+
+  if (!imageUrl) {
+    Providers.globalProvider.graph.client
+      .api(`/groups/${group.id}/photo/$value`)
+      .responseType(ResponseType.RAW)
+      .get()
+      .then((res: Response): Promise<Blob> => {
+        if (!res.ok) {
+          return Promise.reject(res.statusText);
+        }
+
+        return res.blob();
+      })
+      .then((blob: Blob): void => {
+        const blobUrl = url.createObjectURL(blob);
+        setGroupImage(blobUrl);
+      }, _ => { });
+  }
+
+  if (group.groupTypes && group.groupTypes.indexOf('Unified') > -1) {
+    return <Persona text={group.displayName as string}
+        secondaryText={group.description as string}
+        imageUrl={imageUrl}
+        size={PersonaSize.size56}
+        onClick={openGroup}
+        className="group" />;
+  }
+  else {
+    return <div />;
+  }
+}
+
+const GroupsLoading = (props: MgtTemplateProps) => {
+  return <Spinner label='Loading groups...' labelPosition='right' />
+}
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  Providers.globalProvider = new MsalProvider({
+    clientId: '22ea8a07-7069-4d02-a7a0-c47224a1c401'
+  });
+  Providers.globalProvider.onStateChanged(e => {
+    if (Providers.globalProvider.state !== ProviderState.Loading) {
+      setIsLoggedIn(Providers.globalProvider.state === ProviderState.SignedIn);
+    }
+  });
+  TemplateHelper.setBindingSyntax('[[', ']]');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Login />
       </header>
+      { isLoggedIn &&
+        <Get resource="me/memberOf?$top=500" scopes={["group.read.all"]}>
+          <GroupInfo template="value" />
+          <GroupsLoading template="loading" />
+        </Get>
+      }
     </div>
   );
 }
